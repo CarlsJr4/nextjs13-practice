5d434de5793992bfd280c3f06b192f3513e8774f
Carl Dungca <carl.dungca@yahoo.com>
2023-10-27 12:12:42 -0700

    Next auth login authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import ProductCard from './components/ProductCard';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main>
-      <p>Hello World</p>
+      <p>Hello {session && <span>{session.user!.name}</span>}</p>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
